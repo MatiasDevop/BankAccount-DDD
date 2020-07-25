00385c7a9793303b08df6e3065fb6dcfe8e90061
MatiasDevop <solomatias321@gmail.com>
2020-07-25 20:21:32 +0200

    implementing domain layer and create Bank account, transaction entities

---
+++ BankAccountSystem.Model/InsufficientFundsException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountSystem.Model
+{
+    public class InsufficientFundsException : ApplicationException
+    {
+    }
+}
---
+++ BankAccountSystem.Model/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountSystem.Model
+{
+    public class Transaction
+    {
+        public decimal Deposit { get; internal set; }
+        public decimal Withdrawal { get; internal set; }
+        public string Reference
+        { get; internal set; }
+        public DateTime Date
+        { get; internal set; }
+        public Transaction(decimal deposit, decimal withdrawal, string reference, DateTime date)
+        {
+            this.Deposit = deposit;
+            this.Withdrawal = withdrawal;
+            this.Reference = reference;
+            this.Date = date;
+        }
+
+        
+    }
+}
